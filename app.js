918d210751d5f27ffca6c90687c4d8ddef8e5bc5
lawrencechen0921 <show99520show99520@gmail.com>
2021-05-05 17:17:06 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var data = {
+    title: 'Welcome to AWSLC 2021 Project',
+    selected: null,
+    question: 'Which cryptocurrency you shoud buy on 2021?',
+    choices: [
+      'A) BTC',
+      'B) Dodge',
+      'C) Chia',
+      'D) Ethereum'
+    ]
+  }
+  
+  var Choice = {
+    click: function(n){
+      return function(){
+        data.selected = n
+      }
+    },
+    classes: function(n){
+      if (data.selected === n){
+        return 'active'
+      } else {
+        return ''
+      }
+    },
+    view: function(vnode){
+      var n = vnode.attrs.index
+      return m('.choice',{ class: Choice.classes(n), onclick: Choice.click(n) },
+        m('span.l'),
+        m('span.v',data.choices[n])
+      )
+    }
+  }
+  var App = {
+    submit: function(){
+      m.request({
+          method: "PUT",
+          url: "<API-ENDPOINT>",
+          params: {selected: data.selected},
+      })
+      .then(function(data) {
+        console.log('data',data)
+      })
+    },
+    view: function() {
+      return m('main', [
+        m("h1", data.title),
+        m('article',
+          m('h2','Question:'),
+          m('.question',data.question),
+          m(Choice,{index: 0}),
+          m(Choice,{index: 1}),
+          m(Choice,{index: 2}),
+          m(Choice,{index: 3}),
+          m('.submit',
+            m("button", {onclick: App.submit}, 'Submit')
+          )
+        )
+      ])
+    }
+  }
+  
+  m.mount(document.body, App)
+  
